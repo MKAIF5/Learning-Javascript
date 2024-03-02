0eb93b73b248abb6e08d4f2da4254257621883ab
mohammed kaif <mkaif555123@gmail.com>
2024-03-01 17:52:47 -0800

    js practice

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-console.log("KAIF");
-console.log(10 + 10 + 10);
+// console.log("KAIF");
+// console.log(10 + 10 + 10);
 
-let a = +prompt("Enter value 1");
-let b = +prompt("Enter value 2");
-let plus = a + b;
-console.log(plus);
+// let a = +prompt("Enter value 1");
+// let b = +prompt("Enter value 2");
+// let plus = a + b;
+// console.log(plus);
+
+//const location = [];
