94f52b8b38494761c77ddb94231072283b788c4e
mohammed kaif <mkaif555123@gmail.com>
2024-03-07 21:42:19 -0800

    js practice

--- script.js
+++ script.js
@@ -11,13 +11,13 @@
 // let a = "i am a \"web\" develpor"
 // console.log(a);
 // Data Types
-let str = "Hello"; //string
-let nr = 7; //number
-let bigNr = 12345678901234n; //bigint
-let bool = true; //boolean
-let sym = Symbol("unique"); //symbol
-let undef = undefined; //undefined
-let unknown = null; //null
+// let str = "Hello"; //string
+// let nr = 7; //number
+// let bigNr = 12345678901234n; //bigint
+// let bool = true; //boolean
+// let sym = Symbol("unique"); //symbol
+// let undef = undefined; //undefined
+// let unknown = null; //null
 
 // let str1 = "Hi";
 // let nr3 = 3;
@@ -29,3 +29,28 @@ let unknown = null; //null
 // console.log(result2min);
 // console.log(result2mul);
 // console.log(result2div);
+
+
+
+// let myButtonElement = document.querySelector("#myButton");
+// let myInput = document.querySelector("#myInput");
+
+// myButtonElement.addEventListener("click", () => {
+//   console.log(myInput.value);
+
+//   if (myInput.value > 25) {
+//     console.log("garmi");
+//   } else {
+//     console.log("sardi");
+//   }
+
+// });
+
+// let a = prompt("enter your age")
+// if(a >18){
+//   console.log("true");
+// }
+
+// else{
+//   console.log("false");
+// }
