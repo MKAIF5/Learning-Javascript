984148af50bfde8191400ac587a7896c709703ba
mohammed kaif <mkaif555123@gmail.com>
2024-05-15 21:22:43 +0500

    class code practice

--- script.js
+++ script.js
@@ -262,46 +262,46 @@
 
 //CLASS CODE
 
-// replaceAll with loop
-for (let i = 0; i < myText.length; i++) {
-    if (myText.slice(i, i + 12) === "World War II") {
-      myText = myText.slice(0, i) + "the Second World War" + myText.slice(i + 12);
-    }
-  }
+// // replaceAll with loop
+// for (let i = 0; i < myText.length; i++) {
+//     if (myText.slice(i, i + 12) === "World War II") {
+//       myText = myText.slice(0, i) + "the Second World War" + myText.slice(i + 12);
+//     }
+//   }
   
-  // easy method
-  myText = myText.replaceAll("shehzad", "testing");
+//   // easy method
+//   myText = myText.replaceAll("shehzad", "testing");
   
-  let myName = "shehzad";
-  console.log("myText: ", myText);
-  console.log("shehzad".charAt(0));
-  // charAt is same as => "shehzad"[2]
+//   let myName = "shehzad";
+//   console.log("myText: ", myText);
+//   console.log("shehzad".charAt(0));
+//   // charAt is same as => "shehzad"[2]
   
-  console.log(myName[myName.length - 1]);
+//   console.log(myName[myName.length - 1]);
   
-  let dayOfWk = "Sun";
+//   let dayOfWk = "Sun";
   
-  switch (dayOfWk) {
-    case "sat":
-      console.log("kAM KARO");
-      break;
+//   switch (dayOfWk) {
+//     case "sat":
+//       console.log("kAM KARO");
+//       break;
   
-    case "sun":
-      console.log("Coding KARO");
-      break;
+//     case "sun":
+//       console.log("Coding KARO");
+//       break;
   
-    case "fri":
-      console.log("Jummah pharo!");
-      break;
+//     case "fri":
+//       console.log("Jummah pharo!");
+//       break;
   
-    default:
-      console.log("Error aagya hy!");
-  }
+//     default:
+//       console.log("Error aagya hy!");
+//   }
   
-  {
-    const myFunction = () => {
-      console.log("chala function", window.location.href);
-    };
-  }
+//   {
+//     const myFunction = () => {
+//       console.log("chala function", window.location.href);
+//     };
+//   }
   
-  myFunction();
+//   myFunction();
