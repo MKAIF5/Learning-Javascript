7dc7983f4d46951672bcf180c1728dc03fb5011d
mohammed kaif <mkaif555123@gmail.com>
2024-07-10 16:13:04 +0500

    js data types folder make

---
+++ JS-Data-Types/script.js
@@ -0,0 +1,31 @@
+// console.log("KAIF");
+// console.log(10 + 10 + 10);
+
+let a = +prompt("Enter value 1");
+let b = +prompt("Enter value 2");
+let plus = a + b;
+// console.log(plus);
+
+const location = [];
+console.log(Math.floor(Math.random() * 100));
+let ab = "i am a \"web\" develpor"
+// console.log(a);
+// Data Types
+let str = "Hello"; //string
+let nr = 7; //number
+let bigNr = 12345678901234n; //bigint
+let bool = true; //boolean
+let sym = Symbol("unique"); //symbol
+let undef = undefined; //undefined
+let unknown = null; //null
+
+let str1 = "Hi";
+let nr3 = 3;
+let result2add = str1 + nr3;
+let result2min = str1 - nr3;
+let result2mul = str1 * nr3;
+let result2div = str1 / nr3;
+// console.log(result2add);
+// console.log(result2min);
+// console.log(result2mul);
+// console.log(result2div);
