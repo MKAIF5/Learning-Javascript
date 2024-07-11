f9ab067c993f0f983214461cc9b5f19a0551d1f0
mohammed kaif <mkaif555123@gmail.com>
2024-07-11 16:11:37 +0500

    if else logic practice

--- script.js
+++ script.js
@@ -462,3 +462,16 @@
 //     }
 //     console.log(i);
 // } 
+
+let num1, num2;
+if(parseInt(num1 ,10) > parseInt(num2 , 10)){
+    console.log(`The larger of ${num1} and ${num2} is ${num1} .`);
+}
+
+else if(parseInt(num2 , 10) > parseInt(num1 , 10)){
+    console.log(`the larger number ${num1} and ${num2} is ${num2}`);
+}
+
+else{
+    console.log(`The value is ${num1} and ${num2} are equal`);
+}
